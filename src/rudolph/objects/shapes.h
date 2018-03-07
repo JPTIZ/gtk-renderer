7d1af2309c7a252d434eb9e340ca246ca6cde7ea
diogojr2d <diogojrdesouza@gmail.com>
2018-03-07 20:42:38 -0300

    shapes basic structure

--- src/rudolph/objects/shapes.h
+++ src/rudolph/objects/shapes.h
@@ -1,23 +1,55 @@
 #ifndef RUDOLPH_OBJECTS_SHAPES_H
 #define RUDOLPH_OBJECTS_SHAPES_H
 
-#include "../rudolph/render.h"
+#include <vector>
+#include "../render.h"
 
 namespace rudolph::objects {
 
 class Point: render::Drawable {
 public:
+    Point(int x, int y):
+        _x{x}, _y{y}
+    {}
+
     void draw(render::RenderTarget&) const override;
+
+    auto x() const {
+        return _x;
+    }
+
+    auto y() const {
+        return _y;
+    }
+
+private:
+    int _x;
+    int _y;
 };
 
 class Line: render::Drawable {
 public:
+    Line(Point a, Point b):
+        _a{a}, _b{b}
+    {}
+
     void draw(render::RenderTarget&) const override;
+
+private:
+    Point _a;
+    Point _b;
 };
 
 class Polygon: render::Drawable {
 public:
+    Polygon() {
+
+    }
+
     void draw(render::RenderTarget&) const override;
+
+private:
+    std::vector _points
 };
 
 }
